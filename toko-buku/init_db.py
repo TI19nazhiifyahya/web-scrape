a250b54dcc5c8ce3f2f0dbd779f92a0593691fff
Ardian0912 <muhammadnurwidya.2019@student.uny.ac.id>
2022-01-13 14:41:02 +0700

    init_db fix field buku

--- toko-buku/init_db.py
+++ toko-buku/init_db.py
@@ -9,7 +9,7 @@ connection = mysql.connector.connect(
 cur = connection.cursor()
 cur.execute("CREATE DATABASE IF NOT EXISTS toko_buku")
 cur.execute("USE toko_buku")
-cur.execute("CREATE TABLE IF NOT EXISTS buku ( id INT PRIMARY KEY AUTO_INCREMENT, cover TEXT NULL, title TEXT NOT NULL, description TEXT NOT NULL, author TEXT NOT NULL, publisher TEXT NOT NULL,publication_date TEXT NOT NULL, genres TEXT NOT NULL, language TEXT NOT NULL, pages TEXT NOT NULL, compatibility TEXT NOT NULL, price TEXT NOT NULL, rating TEXT NOT NULL, total_rating TEXT NOT NULL )")
+cur.execute("CREATE TABLE IF NOT EXISTS buku ( id INT PRIMARY KEY AUTO_INCREMENT, cover TEXT NULL, title TEXT NOT NULL, description TEXT NOT NULL, author TEXT NOT NULL, publisher TEXT NOT NULL,publication_date DATE NOT NULL, genres TEXT NOT NULL, language TEXT NOT NULL, pages TEXT NOT NULL, compatibility TEXT NOT NULL, price INT NOT NULL, rating TEXT NOT NULL, total_rating TEXT NOT NULL )")
 
 
 connection.commit()
